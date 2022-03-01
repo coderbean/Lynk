87f3db63ccd32bdad3e8dad5ac8cbd3e931a07ea
何若 <jacob.zb@antfin.com>
2022-03-01 15:24:14 +0800

    成功启动了前台服务

--- app/src/main/java/cn/edu/fjnu/musicdemo/BootBroadcastReceiver.java
+++ app/src/main/java/cn/edu/fjnu/musicdemo/BootBroadcastReceiver.java
@@ -3,12 +3,16 @@ package cn.edu.fjnu.musicdemo;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 import android.widget.Toast;
 
 public class BootBroadcastReceiver extends BroadcastReceiver {
+    private final String TAG = "BootBroadcastReceiver";
     @Override
     public void onReceive(Context context, Intent intent) {
         if (Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())) {
+            Log.d(TAG, "收到开机广播");
+            Toast.makeText(context, "收到开机广播", Toast.LENGTH_SHORT).show();
             Intent intent1 = new Intent(context,MainActivity.class);
             intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
             context.startActivity(intent1);
--- app/src/main/java/cn/edu/fjnu/musicdemo/ForegroundService.java
+++ app/src/main/java/cn/edu/fjnu/musicdemo/ForegroundService.java
@@ -1,7 +1,12 @@
 package cn.edu.fjnu.musicdemo;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
 import android.support.v4.content.LocalBroadcastManager;
@@ -13,6 +18,10 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 public class ForegroundService extends Service {
+
+    private NotificationManager notificationManager;
+    private static final String NOTIFICATION_ID = "channedId";
+    private static final String NOTIFICATION_NAME = "channedId";
 
     private Timer timer;
 
@@ -35,6 +44,27 @@ public class ForegroundService extends Service {
                 }
             }
         }, 0L, 800L);
+
+        notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        //创建NotificationChannel
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            NotificationChannel channel = new NotificationChannel(NOTIFICATION_ID, NOTIFICATION_NAME, NotificationManager.IMPORTANCE_HIGH);
+            notificationManager.createNotificationChannel(channel);
+        }
+        startForeground(1,getNotification());
+    }
+
+    private Notification getNotification() {
+        Notification.Builder builder = new Notification.Builder(this)
+                .setSmallIcon(R.drawable.ic_launcher_foreground)
+                .setContentTitle("媒体广播转换服务")
+                .setContentText("正在运行...");
+        //设置Notification的ChannelID,否则不能正常显示
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            builder.setChannelId(NOTIFICATION_ID);
+        }
+        Notification notification = builder.build();
+        return notification;
     }
 
     @Override
